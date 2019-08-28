6f511360feaf9ca4c393bb55df82fcdabe2732cb
Alvaro Pareja-Lecaros <app6141@gmail.com>
2019-08-28 13:15:05 -0400

    perf(test): allow for multiple use cases to be tested at once
    - decreases time to test
    - increases maintainability
      reduces boilerplate for new use cases & new use case examples

--- tests/tests.sh
+++ tests/tests.sh
@@ -12,7 +12,7 @@ FILE="${1:-"$DIR/is.sh"}"
 # read -rst # -n 999 === sleep # without calling an external tool
 # Prepare working directory
 printf 'Warming tests\n' && {
-cd "$(mktemp -d)" || exit 1
+  cd "$(mktemp -d)" || exit 1
 
   : 1> 'forbidden_file'
   chmod 000 'forbidden_file'
@@ -28,185 +28,137 @@ cd "$(mktemp -d)" || exit 1
   ln -s 'dir' 'symlink_dir'
 } && printf '\033[s\033[1F\033[%s@\033[%s@\033[32m\u2713\033[39m\033[u' '' ''
 
-#
 # Helpers
-#
-
-assert_true() {
-    assert_raises "$1" 0
+_assert_raises() {
+  local args expected=$1 condition=$2 && shift 2
+  for args in "${@}"; do assert_raises "$CMD $condition $args" "$expected"; done
 }
 
-assert_false() {
-    assert_raises "$1" 1
-}
+assert_true() { _assert_raises 0 "${1}" "${@:2}"; }
+assert_false() { _assert_raises 1 "${1}" "${@:2}"; }
 
 # shellcheck source=./assert.sh
 . "$DIR/tests/assert.sh"
 
 # Tests
-is="$CMD"
 printf 'Running tests\n' && {
-# is file
-assert_true  "$is file ./file"
-assert_true  "$is file ./symlink_file"
-assert_false "$is file ./dir"
-assert_false "$is file ./symlink_dir"
-assert_false "$is file ./nothing"
+  # is file
+  assert_true  'file' './file' './symlink_file'
+  assert_false 'file' './dir' './symlink_dir' './nothing'
 
-# is directory
-assert_false "$is directory ./file"
-assert_false "$is directory ./symlink_file"
-assert_true  "$is directory ./dir"
-assert_true  "$is directory ./symlink_dir"
-assert_false "$is directory ./nothing"
+  # is directory
+  assert_true  'directory' './dir' './symlink_dir'
+  assert_false 'directory' './file' './symlink_file' './nothing'
 
-# is link
-assert_false "$is link ./file"
-assert_true  "$is link ./symlink_file"
-assert_false "$is link ./dir"
-assert_true  "$is link ./symlink_dir"
-assert_false "$is link ./nothing"
+  # is link
+  assert_false 'link' './file' './dir' './nothing'
+  assert_true  'link' './symlink_file' './symlink_dir'
 
-# is existent
-assert_true  "$is existent ./file"
-assert_true  "$is existent ./symlink_file"
-assert_true  "$is existent ./dir"
-assert_true  "$is existent ./symlink_dir"
-assert_false "$is existent ./nothing"
+  # is existent
+  assert_true  'existent' './file' './symlink_file' './dir' './symlink_dir'
+  assert_false 'existent' './nothing'
 
-# is writable
-assert_true  "$is writeable ./file"
-assert_false "$is writeable ./forbidden_file"
+  # is writable
+  assert_true  'writeable' './file'
+  assert_false 'writeable' './forbidden_file'
 
-# is readable
-assert_true  "$is readable ./file"
-assert_false "$is readable ./forbidden_file"
+  # is readable
+  assert_true  'readable' './file'
+  assert_false 'readable' './forbidden_file'
 
-# is executable
-assert_true  "$is executable ./file"
-assert_false "$is executable ./forbidden_file"
+  # is executable
+  assert_true  'executable' './file'
+  assert_false 'executable' './forbidden_file'
 
-# is available
-assert_true  "$is available which"
-assert_false "$is available witch"
+  # is available
+  assert_true  'available' 'which'
+  assert_false 'available' 'witch'
 
-# is empty
-assert_true  "$is empty"
-assert_true  "$is empty ''"
-assert_false "$is empty abc"
+  # is empty
+  assert_true  'empty' '' '""'
+  assert_false 'empty' 'abc'
 
-# is number
-assert_true  "$is number 123"
-assert_true  "$is number 123.456"
-assert_false "$is number abc"
-assert_false "$is number 123ff"
-assert_false "$is number 123,456"
-assert_false "$is number 12e3"
+  # is number
+  assert_true  'number' '123' '123.456' '-123' '+123'
+  assert_false 'number' 'abc' '123ff' '123,456' '12e3' '+-123'
 
-# is older
-assert_true  "$is older ./old_file ./new_file"
-assert_false "$is older ./new_file ./old_file"
+  # is older
+  assert_true  'older' './old_file ./new_file'
+  assert_false 'older' './new_file ./old_file'
 
-# is newer
-assert_false "$is newer ./old_file ./new_file"
-assert_true  "$is newer ./new_file ./old_file"
+  # is newer
+  assert_false 'newer' './old_file ./new_file'
+  assert_true  'newer' './new_file ./old_file'
 
-# is gt
-assert_false "$is gt 111 222.0"
-assert_false "$is gt 222 222.0"
-assert_true  "$is gt 333 222.0"
-assert_false "$is gt abc 222"
-assert_false "$is gt 222 abc"
-assert_false "$is gt abc abc"
+  # is gt
+  assert_true  'gt' '333 222.0'
+  assert_false 'gt' '222 222.0' '111 222.0' 'abc 222' '222 abc' 'abc abc'
 
-# is lt
-assert_true  "$is lt 111 222.0"
-assert_false "$is lt 222 222.0"
-assert_false "$is lt 333 222.0"
-assert_false "$is lt abc 222"
-assert_false "$is lt 222 abc"
-assert_false "$is lt abc abc"
+  # is lt
+  assert_true  'lt' '111 222.0'
+  assert_false 'lt' '222 222.0' '333 222.0' 'abc 222' '222 abc' 'abc abc'
 
-# is ge
-assert_false "$is ge 111 222.0"
-assert_true  "$is ge 222 222.0"
-assert_true  "$is ge 333 222.0"
-assert_false "$is ge abc 222"
-assert_false "$is ge 222 abc"
-assert_false "$is ge abc abc"
+  # is ge
+  assert_true  'ge' '333 222.0' '222 222.0'
+  assert_false 'ge' '111 222.0' 'abc 222' '222 abc' 'abc abc'
 
-# is le
-assert_true  "$is le 111 222.0"
-assert_true  "$is le 222 222.0"
-assert_false "$is le 333 222.0"
-assert_false "$is le abc 222"
-assert_false "$is le 222 abc"
-assert_false "$is le abc abc"
+  # is le
+  assert_true  'le' '111 222.0' '222 222.0'
+  assert_false 'le' '333 222.0' 'abc 222' '222 abc' 'abc abc'
 
-# is equal
-assert_false "$is equal 111 222.0"
-assert_true  "$is equal 222 222.0"
-assert_false "$is equal 333 222.0"
-assert_false "$is equal abc 222"
-assert_false "$is equal 222 abc"
-assert_true  "$is equal abc abc"
+  # is equal
+  assert_true  'equal' 'abc abc' '222 222.0'
+  assert_false 'equal' '333 222.0' '111 222.0' 'abc 222' '222 abc'
 
-# is matching
-assert_true  "$is matching '[a-c]+' 'abc'"
-assert_false "$is matching '[a-c]+' 'Abc'"
-assert_false "$is matching '[a-c]+' 'abd"
+  # is matching
+  assert_true  'matching' '"[a-c]+" "abc"'
+  assert_false 'matching' '"[a-c]+" "Abc"' '"[a-c]+" "abd"'
 
-# is substring
-assert_true  "$is substring cde abcdef"
-assert_false "$is substring cdf abcdef"
+  # is substring
+  assert_true  'substring' 'cde abcdef'
+  assert_false 'substring' 'cdf abcdef'
 
-# is true
-assert_false "$is true abc"
-assert_true  "$is true true"
-assert_true  "$is true 0"
-assert_false "$is true 1"
-assert_false "$is true -12"
+  # is true
+  assert_true  'true' 'true' '0'
+  assert_false 'true' 'abc' '1' '-12'
 
-# is false
-assert_true  "$is false abc"
-assert_false "$is false true"
-assert_false "$is false 0"
-assert_true  "$is false 1"
-assert_true  "$is false -12"
+  # is false
+  assert_true  'false' 'abc' '1' '-12'
+  assert_false 'false' 'true' '0'
 
-# negation
-assert_true  "$is not number abc"
-assert_false "$is not number 123"
-assert_true  "$is not equal abc def"
-assert_false "$is not equal abc abc"
+  # negation
+  assert_true  'not number' 'abc'
+  assert_true  'not equal' 'abc def'
+  assert_false 'not number' '123'
+  assert_false 'not equal' 'abc abc'
 
-# articles
-assert_true  "$is a number 123"
-assert_true  "$is an number 123"
-assert_true  "$is the number 123"
-assert_true  "$is not a number abc"
-assert_true  "$is not an number abc"
-assert_true  "$is not the number abc"
+  # articles
+  assert_true  'a number' '123'
+  assert_true  'an number' '123'
+  assert_true  'the number' '123'
+  assert_true  'not a number' 'abc'
+  assert_true  'not an number' 'abc'
+  assert_true  'not the number' 'abc'
 
-# test aliases
-assert_true  "$is dir ./dir"
-assert_true  "$is symlink ./symlink_file"
-assert_true  "$is existing ./file"
-assert_true  "$is exist ./file"
-assert_true  "$is exists ./file"
-assert_true  "$is eq 222 222.0"
-assert_true  "$is match '^[a-c]+$' 'abc'"
-assert_true  "$is substr cde abcdef"
-assert_true  "$is installed which"
+  # test aliases
+  assert_true  'dir' './dir'
+  assert_true  'symlink' './symlink_file'
+  assert_true  'existing' './file'
+  assert_true  'exist' './file'
+  assert_true  'exists' './file'
+  assert_true  'eq' '222 222.0'
+  assert_true  'match' '"^[a-c]+$" "abc"'
+  assert_true  'substr' 'cde abcdef'
+  assert_true  'installed' 'which'
 
-# --version
-assert_true "$is --version"
+  # --version
+  assert_true '--version'
 
-# help
-assert_true "$is --help"
+  # help
+  assert_true '--help'
 
-# unknown condition
-assert_false "$is spam foo bar"
+  # unknown condition
+  assert_false 'spam' 'foo bar'
 } && printf '\033[s\033[1F\033[%s@\033[%s@\033[32m\u2713\033[39m\033[u' '' ''
 
 # end of tests
