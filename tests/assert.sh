fed58272fc8109ab3d28737f262eba5d4034c682
Alvaro Pareja-Lecaros <app6141@gmail.com>
2019-09-05 11:36:54 -0400

    style(assert): vscode sh syntax highlighting messes up w/ previous continuation

--- tests/assert.sh
+++ tests/assert.sh
@@ -74,9 +74,9 @@ assert_end() {
     # assert_end [suite ..]
     tests_endtime="$(date +%s%N)"
     # required visible decimal place for seconds (leading zeros if needed)
-    local tests_time="$( \
-        printf "%010d" "$(( ${tests_endtime/%N/000000000} 
-                            - ${tests_starttime/%N/000000000} ))")"  # in ns
+    local tests_time="$(printf "%010d" \
+        "$(( ${tests_endtime/%N/000000000} - ${tests_starttime/%N/000000000} ))"
+    )"  # in ns
     tests="$tests_ran ${*:+$* }tests"
     [[ -n "$DISCOVERONLY" ]] && echo "collected $tests." && _assert_reset && return
     [[ -n "$DEBUG" ]] && echo
